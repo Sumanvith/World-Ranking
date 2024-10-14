8cbe7f0dda319376082e8a1f822580d1e945e5df
Sumanvith <dellbilla@gmail.com>
2024-10-14 23:09:30 +0530

    Added Router for Body and CountryPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import Header from "./components/Header";
+import Body from "./components/Body";
+import CountryPage from "./components/CountryPage";
+
+import {
+  createBrowserRouter,
+  Outlet,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className="bg-[#282B30] h-screen w-screen">
       <Header></Header>
+      <Outlet></Outlet>
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout></AppLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Body></Body>,
+      },
+      {
+        path: "/country/:country",
+        element: <CountryPage></CountryPage>,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
